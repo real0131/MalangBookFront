1d40ec9d7070d68ca2623ef484e110fc52fc3e2e
Jaeho Cho <real0131@gmail.com>
2016-10-20 15:39:07 +0900

    add login click event

---
+++ untitled/js/login.js
@@ -0,0 +1,48 @@
+list_1 = function () {
+    $(".login-nav-li")
+        .css("background-color","#989898")
+        .css("color","white");
+    $("#list-1")
+        .css("background-color","white")
+        .css("color","#535353");
+    $("#login-login").css("display","inline-block");
+    $("#login-findID").css("display","none");
+    $("#login-findPW").css("display","none");
+    $("#login-signUP").css("display","none");
+};
+list_2 = function () {
+    $(".login-nav-li")
+        .css("background-color","#989898")
+        .css("color","white");
+    $("#list-2")
+        .css("background-color","white")
+        .css("color","#535353");
+    $("#login-login").css("display","none");
+    $("#login-findID").css("display","inline-block");
+    $("#login-findPW").css("display","none");
+    $("#login-signUP").css("display","none");
+};
+list_3 = function () {
+    $(".login-nav-li")
+        .css("background-color","#989898")
+        .css("color","white");
+    $("#list-3")
+        .css("background-color","white")
+        .css("color","#535353");
+    $("#login-login").css("display","none");
+    $("#login-findID").css("display","none");
+    $("#login-findPW").css("display","inline-block");
+    $("#login-signUP").css("display","none");
+};
+list_4 = function () {
+    $(".login-nav-li")
+        .css("background-color","#989898")
+        .css("color","white");
+    $("#list-4")
+        .css("background-color","white")
+        .css("color","#535353");
+    $("#login-login").css("display","none");
+    $("#login-findID").css("display","none");
+    $("#login-findPW").css("display","none");
+    $("#login-signUP").css("display","inline-block");
+};
