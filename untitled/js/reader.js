0f6a7ba1a07b53947fabb2d97692086777c5c513
Jaeho Cho <real0131@gmail.com>
2016-11-01 02:30:47 +0900

    add reply event

---
+++ untitled/js/reader.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+   $('#reply-textarea').on('keyup',function () {
+      if($(this).val().length > 200){
+         $(this.val($(this).val().substring(0,200)));
+      }
+      $('#reply-textare-length').text($('#reply-textarea').val().length+"/200");
+   });
+   $('#reply-textarea').keyup(function(e){
+      if(e.keyCode == 8) {
+         $('#reply-textare-length').text($('#reply-textarea').val().length+"/200");
+      }
+   });
+});
