262addb71968fd84bd21baec9f16ba751a010eef
Jaeho Cho <real0131@gmail.com>
2016-11-04 02:32:08 +0900

    add login page

---
+++ untitled/js/main.js
@@ -0,0 +1,12 @@
+$('#login-menu-signup').click(function () {
+    $('.login-view').css({display:'block', position : 'fixed',top:'0',left:'0',transition : "background-color 0.5s ease"});
+});
+$('#login-menu-findid').click(function () {
+    $('.login-view').css({display:'block', position : 'fixed',top:'0',left:'0',transition : "background-color 0.5s ease"});
+});
+$('#login-menu-findpw').click(function () {
+    $('.login-view').css({display:'block', position : 'fixed',top:'0',left:'0',transition : "background-color 0.5s ease"});
+});
+$('.login-nav-cancel').click(function () {
+    $('.login-view').css({display:'none', position : 'fixed',top:'0',left:'0',transition : "background-color 0.5s ease"});
+});
