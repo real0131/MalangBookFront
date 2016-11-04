1533a3bf9d8da659852b55002129fc1083892ea1
Jaeho Cho <real0131@gmail.com>
2016-11-04 17:15:09 +0900

    fix login

--- untitled/js/main.js
+++ untitled/js/main.js
@@ -1,12 +1,16 @@
 $('#login-menu-signup').click(function () {
     $('.login-view').css({display:'block', position : 'fixed',top:'0',left:'0',transition : "background-color 0.5s ease"});
+    $('.content-wrapper').css({display:'none'});
 });
 $('#login-menu-findid').click(function () {
     $('.login-view').css({display:'block', position : 'fixed',top:'0',left:'0',transition : "background-color 0.5s ease"});
+    $('.content-wrapper').css({display:'none'});
 });
 $('#login-menu-findpw').click(function () {
     $('.login-view').css({display:'block', position : 'fixed',top:'0',left:'0',transition : "background-color 0.5s ease"});
+    $('.content-wrapper').css({display:'none'});
 });
 $('.login-nav-cancel').click(function () {
     $('.login-view').css({display:'none', position : 'fixed',top:'0',left:'0',transition : "background-color 0.5s ease"});
+    $('.content-wrapper').css({display:'block'});
 });
