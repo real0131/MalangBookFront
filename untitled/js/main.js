38997c0df7e537e1b025653215c5b75fe8706512
Jaeho Cho <real0131@gmail.com>
2016-11-04 17:20:36 +0900

    fix login link

--- untitled/js/main.js
+++ untitled/js/main.js
@@ -1,13 +1,44 @@
 $('#login-menu-signup').click(function () {
     $('.login-view').css({display:'block', position : 'fixed',top:'0',left:'0',transition : "background-color 0.5s ease"});
+    $(".login-nav-li")
+        .css("background-color","#989898")
+        .css("color","white");
+    $("#list-4")
+        .css("background-color","white")
+        .css("color","#535353");
+    $("#login-login").css("display","none");
+    $("#login-findID").css("display","none");
+    $("#login-findPW").css("display","none");
+    $("#login-signUP").css("display","inline-block");
     $('.content-wrapper').css({display:'none'});
 });
 $('#login-menu-findid').click(function () {
     $('.login-view').css({display:'block', position : 'fixed',top:'0',left:'0',transition : "background-color 0.5s ease"});
+
     $('.content-wrapper').css({display:'none'});
+    $(".login-nav-li")
+        .css("background-color","#989898")
+        .css("color","white");
+    $("#list-2")
+        .css("background-color","white")
+        .css("color","#535353");
+    $("#login-login").css("display","none");
+    $("#login-findID").css("display","inline-block");
+    $("#login-findPW").css("display","none");
+    $("#login-signUP").css("display","none");
 });
 $('#login-menu-findpw').click(function () {
     $('.login-view').css({display:'block', position : 'fixed',top:'0',left:'0',transition : "background-color 0.5s ease"});
+    $(".login-nav-li")
+        .css("background-color","#989898")
+        .css("color","white");
+    $("#list-3")
+        .css("background-color","white")
+        .css("color","#535353");
+    $("#login-login").css("display","none");
+    $("#login-findID").css("display","none");
+    $("#login-findPW").css("display","inline-block");
+    $("#login-signUP").css("display","none");
     $('.content-wrapper').css({display:'none'});
 });
 $('.login-nav-cancel').click(function () {
