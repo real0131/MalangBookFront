7d01452923657f41420676068eecef72e03e6da6
Jaeho Cho <real0131@gmail.com>
2016-11-10 00:34:01 +0900

    add nav login at main.html

--- untitled/js/main.js
+++ untitled/js/main.js
@@ -45,3 +45,17 @@ $('.login-nav-cancel').click(function () {
     $('.login-view').css({display:'none', position : 'fixed',top:'0',left:'0',transition : "background-color 0.5s ease"});
     $('.content-wrapper').css({display:'block'});
 });
+$('#nav-li-last').click(function () {
+    $('.login-view').css({display:'block', position : 'fixed',top:'0',left:'0',transition : "background-color 0.5s ease"});
+    $(".login-nav-li")
+        .css("background-color","#989898")
+        .css("color","white");
+    $("#list-3")
+        .css("background-color","white")
+        .css("color","#535353");
+    $("#login-login").css("display","inline-block");
+    $("#login-findID").css("display","none");
+    $("#login-findPW").css("display","none");
+    $("#login-signUP").css("display","none");
+    $('.content-wrapper').css({display:'none'});
+});
